e2f138980bc947a8ee03cd402abf72c236d4b05b
Angular CLI <angular-cli@angular.io>
2019-04-12 17:16:25 +0700

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { CircleCiAngPage } from './app.po';
+
+describe('circle-ci-ang App', () => {
+  let page: CircleCiAngPage;
+
+  beforeEach(() => {
+    page = new CircleCiAngPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
