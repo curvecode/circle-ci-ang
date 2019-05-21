06dab46960401514a5d544d9cedeeccbbda9ccec
AAVN\ltthanh <curvecode@gmail.com>
2019-05-21 08:28:19 +0700

    Routing and page component in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './page/blog/blog.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { AboutComponent } from './page/about/about.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: []
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+    children: []
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false, // Enable or disable hash tag on URL
+    enableTracing: true // Toggles whether the router should log all navigation events to the console
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,25 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './page/login/login.component';
+import { HomeComponent } from './page/home/home.component';
+import { BlogComponent } from './page/blog/blog.component';
+import { AboutComponent } from './page/about/about.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    BlogComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
