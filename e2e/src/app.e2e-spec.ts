27e0641f328b02853ce8b72e83de35acfcf84507
AAVN\ltthanh <curvecode@gmail.com>
2019-12-05 17:12:57 +0700

    Add run e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('circle-ci-ang App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Lorem Ipsum');
   });
 });
